aa5ed5f31651673bbc744a906e14c8a03456a651
Lily Smith <81171629+lilysmith1206@users.noreply.github.com>
2025-02-03 15:17:06 -0500

    Add authorization via static token header validaton middleware

--- TechHive Solutions User Management API/TechHive Solutions User Managemen API/Program.cs
+++ TechHive Solutions User Management API/TechHive Solutions User Managemen API/Program.cs
@@ -1,8 +1,27 @@
+using Microsoft.Extensions.Primitives;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using TechHive_Solutions_User_Management_API.Middleware;
 using TechHive_Solutions_User_Management_API.Repositories;
 
 namespace TechHive_Solutions_User_Management_API
 {
+    public class HeaderTokenFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "token",
+                In = ParameterLocation.Header,
+                Required = false
+            });
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
@@ -14,7 +33,9 @@ namespace TechHive_Solutions_User_Management_API
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(config => {
+                config.OperationFilter<HeaderTokenFilter>();
+            });
 
             builder.Services.AddSingleton<IUserRepository, UserRepository>();
 
@@ -33,6 +54,29 @@ namespace TechHive_Solutions_User_Management_API
             }
 
             app.UseExceptionHandler();
+
+            app.Use((context, next) =>
+            {
+                StringValues stringValues = context.Request.Headers["token"];
+
+                if (stringValues == "J1Y,-&Fk?U^6]0nFpP_>x7}^352W5;!")
+                {
+                    return next.Invoke();
+                }
+                else
+                {
+                    context.Response.StatusCode = 401;
+
+                    return Task.CompletedTask;
+                }
+            });
+
+            app.Use((context, next) =>
+            {
+                Console.WriteLine($"{context.Request.Method} - {context.Request.Path} - {context.Response.StatusCode}");
+
+                return next.Invoke();
+            });
 
             app.UseHttpsRedirection();
 
